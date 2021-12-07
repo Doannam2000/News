ded1168918edee9e8786d08e91402d2d1360be5c
DoanNam2000 <doanduynam2000@gmail.com>
2021-12-07 12:26:46 +0700

    update passwordScreen

---
+++ app/src/main/java/com/ddwan/news/PasswordActivity.kt
@@ -0,0 +1,51 @@
+package com.ddwan.news
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.ddwan.news.viewmodel.PassViewModel
+import kotlinx.android.synthetic.main.activity_password.*
+
+class PasswordActivity : AppCompatActivity() {
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[PassViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+        supportActionBar?.hide()
+        observe()
+        btn0.setOnClickListener { viewModel.checkPass(0) }
+        btn1.setOnClickListener { viewModel.checkPass(1) }
+        btn2.setOnClickListener { viewModel.checkPass(2) }
+        btn3.setOnClickListener { viewModel.checkPass(3) }
+        btn4.setOnClickListener { viewModel.checkPass(4) }
+        btn5.setOnClickListener { viewModel.checkPass(5) }
+        btn6.setOnClickListener { viewModel.checkPass(6) }
+        btn7.setOnClickListener { viewModel.checkPass(7) }
+        btn8.setOnClickListener { viewModel.checkPass(8) }
+        btn9.setOnClickListener { viewModel.checkPass(9) }
+        btnC.setOnClickListener { viewModel.checkPass(10) }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun observe() {
+        viewModel.check.observe(this, {
+            when (it) {
+                0 -> textDisplay.text = "Nhập mật khẩu"
+                1 -> startActivity(Intent(this,
+                    MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK))
+                2 -> textDisplay.text = "Mật khẩu không đúng"
+                3 -> textDisplay.text = "Thay đổi thành công"
+                4 -> textDisplay.text = "Nhập mật khẩu mới"
+            }
+        })
+        viewModel.textView.observe(this, {
+            textPass.text = it
+        })
+    }
+}
---
+++ app/src/main/java/com/ddwan/news/viewmodel/PassViewModel.kt
@@ -0,0 +1,81 @@
+package com.ddwan.news.viewmodel
+
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class PassViewModel(application: Application) : AndroidViewModel(application) {
+
+    var check = MutableLiveData<Int>()
+    var textView = MutableLiveData<String>()
+    var password = ""
+    var pass = ""
+    var isFirst = true
+    private val sharedPreferences: SharedPreferences =
+        application.getSharedPreferences("SHARE_PREFERENCES", Context.MODE_PRIVATE)
+
+    init {
+        password = sharedPreferences.getString("key", "")!!
+        isFirst = sharedPreferences.getBoolean("isFirst", true)
+        if (isFirst)
+            check.value = 4
+
+    }
+
+    fun checkPass(i: Int) {
+        when (i) {
+            0 -> if (pass.length < 4) pass += 0
+            1 -> if (pass.length < 4) pass += 1
+            2 -> if (pass.length < 4) pass += 2
+            3 -> if (pass.length < 4) pass += 3
+            4 -> if (pass.length < 4) pass += 4
+            5 -> if (pass.length < 4) pass += 5
+            6 -> if (pass.length < 4) pass += 6
+            7 -> if (pass.length < 4) pass += 7
+            8 -> if (pass.length < 4) pass += 8
+            9 -> if (pass.length < 4) pass += 9
+            10 -> if (pass.isNotEmpty()) pass = pass.substring(0, pass.length - 1)
+        }
+        changeText()
+    }
+
+    private fun changeText() {
+        when (pass.length) {
+            0 -> textView.value = ""
+            1 -> textView.value = "*"
+            2 -> textView.value = "* *"
+            3 -> textView.value = "* * *"
+            4 -> {
+                textView.value = "* * * *"
+                if (isFirst) {
+                    password = pass
+                    sharedPreferences.edit().putString("key", pass).apply()
+                    sharedPreferences.edit().putBoolean("isFirst", false).apply()
+                    isFirst = false
+                    check.value = 3
+                    GlobalScope.launch {
+                        delay(1000)
+                        check.postValue(1)
+                    }
+                } else {
+                    if (pass == password) {
+                        check.value = 1
+                    } else {
+                        check.value = 2
+                        GlobalScope.launch {
+                            delay(1000)
+                            pass = ""
+                            textView.postValue("")
+                            check.postValue(0)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
